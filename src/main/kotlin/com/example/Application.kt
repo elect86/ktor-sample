24770a4f31d90296e71bc206ef61d04241021905
Giuseppe Barbieri <elect86@gmail.com>
2024-03-07 16:48:22 +0100

    bug

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -18,7 +18,7 @@ import net.pwall.json.schema.JSONSchema
 
 // change port
 fun main() {
-    embeddedServer(Netty, port = 8082, host = "127.0.0.1", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
         .start(wait = true)
 }
 
@@ -32,6 +32,8 @@ fun Application.module() {
 fun Application.configureRouting() {
     routing {
         post("/webhooks/format") {
+
+            val DATASETjson = "DATASET.json"
 
             /*
             BodySchema
@@ -65,16 +67,29 @@ fun Application.configureRouting() {
 
             val api = BranchesApi(defaultClient)
             val diffList = api.diffBranch(bodySchema.repositoryId, bodySchema.branchId).execute()
+            diffList.results.forEach { println(it.path) }
+
+            val files = diffList.results.filter { it.type == Diff.TypeEnum.ADDED }.map { it.path }
+            fun check(files: List<String>) {
+//                println("[check] $files")
+                val (dirs, normalFiles) = files.partition { '/' in it }
+                if (DATASETjson in normalFiles)
+                    return
+                if (dirs.isNotEmpty())
+                    check(dirs.map { it.substringAfter('/') })
+            }
+
+            check(files)
             val datasetDiffs = diffList.results.filter {
-                it.type == Diff.TypeEnum.ADDED && it.path.substringAfterLast('/') == "DATASET.json"
+                it.type == Diff.TypeEnum.ADDED && it.path.substringAfterLast('/') == DATASETjson
             }
             if (datasetDiffs.size > 1) {
-                call.respond(io.ktor.http.HttpStatusCode.NotAcceptable, "multiple DATASET.json found\n")
+                call.respond(io.ktor.http.HttpStatusCode.NotAcceptable, "multiple $DATASETjson found\n")
                 return@post
             }
             val datasetDiff = datasetDiffs.firstOrNull()
             if (datasetDiff == null) {
-                call.respond(io.ktor.http.HttpStatusCode.NotFound, "DATASET.json not found\n")
+                call.respond(io.ktor.http.HttpStatusCode.NotFound, "$DATASETjson not found\n")
                 return@post
             }
 
@@ -84,9 +99,9 @@ fun Application.configureRouting() {
             val validation = schema.validate(datasetFile.readText())
 
             if (!validation)
-                call.respond(io.ktor.http.HttpStatusCode.NotAcceptable, "DATASET.json is not conformant\n")
+                call.respond(io.ktor.http.HttpStatusCode.NotAcceptable, "$DATASETjson is not conformant\n")
             else
-                call.respond(io.ktor.http.HttpStatusCode.OK, "DATASET.json found and conformant\n")
+                call.respond(io.ktor.http.HttpStatusCode.OK, "$DATASETjson found and conformant\n")
         }
     }
 }
